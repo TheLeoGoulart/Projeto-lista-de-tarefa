889bad55c4f020912b50ae86c2769cbaf1e928fa
TheLeoGoulart <Leonardo.p.goulart4@gmail.com>
2022-09-11 21:05:41 -0300

    Implementação da Novidade

--- Tarefa/build/index.js
+++ Tarefa/build/index.js
@@ -1,33 +1,59 @@
 "use strict";
 let listElement = document.querySelector("#app ul");
-let inputElement = document.querySelector("#app input");
+let inputElement = document.querySelector("#app input#tarefa");
 let buttonElement = document.querySelector("#app button");
+let radioElementU = document.querySelector("#app input#urgente");
+let radioElementA = document.querySelector("#app input#atencao");
+let radioElementS = document.querySelector("#app input#sem_prazo");
 let listaSalva = localStorage.getItem("@listagem_tarefas");
 let tarefas = listaSalva !== null && JSON.parse(listaSalva) || [];
 function listarTarefas() {
     listElement.innerHTML = "";
-    tarefas.map(item => {
-        let todoElement = document.createElement("li");
-        let tarefaText = document.createTextNode(item);
-        todoElement.appendChild(tarefaText);
-        listElement.appendChild(todoElement);
+    tarefas.map(function (item, cont) {
+        const ant = cont - 1;
+        if (item === " <URGENTE> ") {
+            document.getElementById(`${ant}`).style.color = "#f00";
+        }
+        else if (item === " <ATENÇÂO> ") {
+            document.getElementById(`${ant}`).style.color = "rgba(233, 161, 6, 0.625)";
+        }
+        else if (item === " <SEM PRAZO> ") {
+            document.getElementById(`${ant}`).style.color = "rgb(15, 92, 21)";
+        }
+        else {
+            let todoElement = document.createElement("li");
+            todoElement.id = `${cont}`;
+            let tarefaText = document.createTextNode(item);
+            todoElement.appendChild(tarefaText);
+            listElement.appendChild(todoElement);
+        }
     });
 }
 listarTarefas();
-function adcicionarTarefa() {
+function adicionarTarefa() {
     if (inputElement.value === "") {
         alert("Digite alguma tarefa!");
         return false;
     }
     else {
         let tarefaDigitada = inputElement.value;
-        tarefas.push(tarefaDigitada);
+        let prioridade = "";
+        if (radioElementU.checked === true) {
+            prioridade = " <URGENTE> ";
+        }
+        else if (radioElementA.checked === true) {
+            prioridade = " <ATENÇÂO> ";
+        }
+        else if (radioElementS.checked === true) {
+            prioridade = " <SEM PRAZO> ";
+        }
+        tarefas.push(tarefaDigitada, prioridade);
         inputElement.value = "";
         listarTarefas();
         salvarDados();
     }
 }
-buttonElement.onclick = adcicionarTarefa;
+buttonElement.onclick = adicionarTarefa;
 function salvarDados() {
     localStorage.setItem("@listagem_tarefas", JSON.stringify(tarefas));
 }
--- Tarefa/src/index.ts
+++ Tarefa/src/index.ts
@@ -1,31 +1,52 @@
 let listElement = document.querySelector("#app ul") as HTMLUListElement;
-let inputElement = document.querySelector("#app input") as HTMLInputElement;
+let inputElement = document.querySelector("#app input#tarefa") as HTMLInputElement;
 let buttonElement = document.querySelector("#app button") as HTMLElement;
+let radioElementU = document.querySelector("#app input#urgente") as HTMLInputElement;
+let radioElementA = document.querySelector("#app input#atencao") as HTMLInputElement;
+let radioElementS = document.querySelector("#app input#sem_prazo") as HTMLInputElement;
+
 
 let listaSalva: string | null = localStorage.getItem("@listagem_tarefas");
-let tarefas: string[] = listaSalva !== null && JSON.parse(listaSalva) || [];
+let tarefas: [string, number] = listaSalva !== null && JSON.parse(listaSalva) || [];
 
 function listarTarefas(){
     listElement.innerHTML = "";
 
-    tarefas.map (item =>{
-        let todoElement = document.createElement("li");
-        let tarefaText = document.createTextNode(item);
-
-        todoElement.appendChild(tarefaText);
-        listElement.appendChild(todoElement);
+    tarefas.map (function (item, cont){
+        const ant = cont - 1;
+        if (item === " <URGENTE> "){  
+            document.getElementById(`${ant}`)!.style.color = "#f00";
+        } else if (item === " <ATENÇÂO> "){
+            document.getElementById(`${ant}`)!.style.color = "rgba(233, 161, 6, 0.625)";
+        } else if(item === " <SEM PRAZO> "){
+            document.getElementById(`${ant}`)!.style.color = "rgb(15, 92, 21)";
+        } else{
+            let todoElement = document.createElement("li");
+            todoElement.id = `${cont}`;
+            let tarefaText = document.createTextNode(<string>item);
+            todoElement.appendChild(tarefaText);
+            listElement.appendChild(todoElement);
+        }
     })
 }
 
 listarTarefas();
 
-function adcicionarTarefa(){
+function adicionarTarefa(){
     if(inputElement.value === ""){
         alert("Digite alguma tarefa!")
         return false;
     }else{
         let tarefaDigitada: string = inputElement.value;
-        tarefas.push(tarefaDigitada);
+        let prioridade: string = "";
+        if (radioElementU.checked === true){
+            prioridade = " <URGENTE> ";
+        } else if(radioElementA.checked === true){
+            prioridade = " <ATENÇÂO> ";
+        } else if(radioElementS.checked === true){
+            prioridade = " <SEM PRAZO> ";
+        }
+        tarefas.push(tarefaDigitada, prioridade);
 
         inputElement.value = "";
         listarTarefas();
@@ -33,7 +54,7 @@ function adcicionarTarefa(){
     }
 }
 
-buttonElement.onclick = adcicionarTarefa
+buttonElement.onclick = adicionarTarefa
 
 function salvarDados(){
     localStorage.setItem("@listagem_tarefas", JSON.stringify(tarefas));
